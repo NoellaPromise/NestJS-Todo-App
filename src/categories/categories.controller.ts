11fd652446307b624aff514c9638e7dae843d903
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-12-14 15:55:19 +0200

    implement unit testing

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  ValidationPipe,
+  // Param,
+  // Delete,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Category } from './category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -12,10 +20,19 @@ export class CategoriesController {
     return this.categoryService.getAllCategories();
   }
 
+  // @Get(':id')
+  // getCategoryById(@Param('id') id: string): Category {
+  //   return this.categoryService.getCategoryById(id);
+  // }
+
   @Post()
   createCategory(
     @Body(ValidationPipe) createCategoryDto: CreateCategoryDto,
   ): Category {
     return this.categoryService.createCategory(createCategoryDto.name);
   }
+  // @Delete(':id')
+  // deleteCategoty(@Param('id') id: string): void {
+  //   this.categoryService.deleteCategory(id);
+  // }
 }
